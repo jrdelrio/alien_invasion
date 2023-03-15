2bf044ce7d2b29847a7c89faf6cc3e5fe51ccc92
jrdelrio <jrdelriodom@gmail.com>
2023-03-15 14:36:58 -0300

    limited the number of bullets
    Ahora falta empezar a ponerle aliens.
    Propongo un upgrade de poner una segunda nave al costado que corra de arriba a abajo

--- settings.py
+++ settings.py
@@ -10,7 +10,8 @@ class Settings:
         self.ship_speed = 3
         
         # bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 3.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
