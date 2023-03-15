d6075030aeb9f1c54db1d7877860cdae4b056c70
jrdelrio <jrdelriodom@gmail.com>
2023-03-15 18:13:53 -0300

    alien created
    next step, alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -27,6 +28,10 @@ class AlienInvasion:
         # create the bullets
         self.bullets = pygame.sprite.Group()
 
+        # create fleet
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
     
     def run_game(self):
         """Start the main loop for the game."""
@@ -40,6 +45,12 @@ class AlienInvasion:
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
+    def _create_fleet(self):
+        '''Create the fleet of aliens'''
+
+        # Make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
     
     def _check_events(self):
         """Respond to keypresses and mouse events"""
@@ -98,6 +109,8 @@ class AlienInvasion:
         # draw the bullets
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+            
+        self.aliens.draw(self.screen)
 
 
 if __name__ == '__main__':
